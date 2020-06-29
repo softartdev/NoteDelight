1ea295b3c52e1b5ab45eac631d99f61064fa4865
artur <artik222012@gmail.com>
2020-06-29 17:32:58 +0500

    fix IosPlatformNoteUseCaseTest

--- shared/src/iosTest/kotlin/com/softartdev/notedelight/shared/db/IosPlatformNoteUseCaseTest.kt
+++ shared/src/iosTest/kotlin/com/softartdev/notedelight/shared/db/IosPlatformNoteUseCaseTest.kt
@@ -1,12 +1,6 @@
 package com.softartdev.notedelight.shared.db
 
-import kotlinx.coroutines.runBlocking
-
 class IosPlatformNoteUseCaseTest : BasePlatformNoteUseCaseTest() {
 
     override val platformRepo: PlatformRepo = IosPlatformRepo()
-
-    override fun <T> runTest(block: suspend () -> T) {
-        runBlocking { block() }
-    }
 }
