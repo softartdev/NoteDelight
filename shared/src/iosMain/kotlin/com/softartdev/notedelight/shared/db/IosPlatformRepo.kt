d48d0256218ab87b2d129b0b08d748142e3b2962
artur <artik222012@gmail.com>
2020-06-30 04:30:13 +0500

    freeze set atomic refs on native common

--- shared/src/iosMain/kotlin/com/softartdev/notedelight/shared/db/IosPlatformRepo.kt
+++ shared/src/iosMain/kotlin/com/softartdev/notedelight/shared/db/IosPlatformRepo.kt
@@ -2,6 +2,7 @@ package com.softartdev.notedelight.shared.db
 
 import com.squareup.sqldelight.db.SqlDriver
 import kotlin.native.concurrent.AtomicReference
+import kotlin.native.concurrent.freeze
 
 class IosPlatformRepo : PlatformRepo() {
 
@@ -11,14 +12,14 @@ class IosPlatformRepo : PlatformRepo() {
     override var driver: SqlDriver?
         get() = _driver.value
         set(value) {
-            _driver.value = value
+            _driver.value = value.freeze()
         }
 
     private val _noteDb = AtomicReference<NoteDb?>(buildDatabaseInstanceIfNeed())
     override var noteDb: NoteDb?
         get() = _noteDb.value
         set(value) {
-            _noteDb.value = value
+            _noteDb.value = value.freeze()
         }
 
     override fun createDriver(): SqlDriver = driverFactory.createDriver()
