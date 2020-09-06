d9e17f321ad5ecd8195a67a11f30d683b6843adc
artur <artik222012@gmail.com>
2020-09-06 05:46:08 +0500

    fix IosDbRepo

--- shared/src/iosTest/kotlin/com/softartdev/notedelight/shared/BaseTest.kt
+++ shared/src/iosTest/kotlin/com/softartdev/notedelight/shared/BaseTest.kt
@@ -1,13 +1,13 @@
 package com.softartdev.notedelight.shared
 
 import com.softartdev.notedelight.shared.database.DatabaseRepo
-import com.softartdev.notedelight.shared.database.IosDbTestRepo
+import com.softartdev.notedelight.shared.database.IosDbRepo
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.runBlocking
 
 actual abstract class BaseTest actual constructor() {
 
-    actual val dbRepo: DatabaseRepo = IosDbTestRepo()
+    actual val dbRepo: DatabaseRepo = IosDbRepo()
 
     actual fun <T> runTest(block: suspend CoroutineScope.() -> T) {
         runBlocking { block() }
