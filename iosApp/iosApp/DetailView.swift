3f3938a2099ebb53fb31c7b402e618beff65d260
artur <artik222012@gmail.com>
2020-09-09 06:58:30 +0500

    edit title

--- iosApp/iosApp/DetailView.swift
+++ iosApp/iosApp/DetailView.swift
@@ -12,7 +12,6 @@ import shared
 struct DetailView: View {
     var noteId: Int64?
     @ObservedObject private(set) var viewModel: DetailViewModel
-    @State var isSaveAlertShowing = false
     
     var body: some View {
         Group {
@@ -33,26 +32,37 @@ struct DetailView: View {
             case .loading:
                 return AnyView(LoadingView())
             case .loaded(let note):
-                return textView(note: note)
+                return AnyView(EditView(noteId: self.noteId, viewModel: self.viewModel, title: note.title, text: note.text))
             case .error(let description):
                 return AnyView(ErrorView(message: description))
         }
     }
-    
-    private func textView(note: Note) -> AnyView {
-        let textView = TextView(text: note.text)
-        return AnyView(textView
-            .navigationBarTitle(note.title)
-            .navigationBarItems(trailing: Button(action: {
-                self.isSaveAlertShowing.toggle()
-            }, label: {
-                Text("Save")
-            }))
+
+    struct EditView: View {
+        var noteId: Int64?
+        var viewModel: DetailViewModel
+        @State var title: String
+        @State var text: String
+        @State var isSaveAlertShowing = false
+        
+        var body: some View {
+            VStack {
+                TextField("Title", text: $title)
+                    .font(.title)
+                    .padding(EdgeInsets(top: 10, leading: 10, bottom: 0, trailing: 10))
+                Spacer()
+                TextView(text: $text)
+            }.navigationBarItems(trailing: Button(action: {
+                    self.isSaveAlertShowing.toggle()
+                }, label: {
+                    Text("Save")
+                }))
             .alert(isPresented: $isSaveAlertShowing) {
                 Alert(title: Text("Save"), message: Text("Are you sure you want to save this?"), primaryButton: .destructive(Text("OK")) {
-                    self.viewModel.saveNote(id: self.noteId!, title: note.title, text: textView.text)
+                    self.viewModel.saveNote(id: self.noteId!, title: self.title, text: self.text)
                 }, secondaryButton: .cancel())
-        })
+            }
+        }
     }
 }
 
