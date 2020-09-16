dcf81ff708b65bafde243be90453cf23ef95bd18
artur <artik222012@gmail.com>
2020-09-16 12:44:21 +0500

    launch notes flow test

--- shared/src/iosTest/kotlin/com/softartdev/notedelight/shared/data/QueryUseCaseTest.kt
+++ shared/src/iosTest/kotlin/com/softartdev/notedelight/shared/data/QueryUseCaseTest.kt
@@ -4,9 +4,15 @@ import com.softartdev.notedelight.shared.BaseTest
 import com.softartdev.notedelight.shared.database.TestSchema
 import com.squareup.sqldelight.runtime.coroutines.asFlow
 import com.squareup.sqldelight.runtime.coroutines.mapToList
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import kotlin.native.concurrent.AtomicInt
+import kotlin.native.concurrent.isFrozen
 import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
 import kotlin.test.Test
@@ -28,6 +34,30 @@ class QueryUseCaseTest : BaseTest() {
     fun tearDown() {
         val noteDb = dbRepo.buildDatabaseInstanceIfNeed().noteDb
         noteDb.noteQueries.deleteAll()
+    }
+
+    @Test
+    fun launchNotesFlow() {
+        val count = AtomicInt(0)
+        val flow = queryUseCase.noteQueries.getAll().asFlow().mapToList()
+        runBlocking {
+            withContext(Dispatchers.Default) {
+                flow.onEach { actNotes ->
+                    withContext(this@runBlocking.coroutineContext) {
+                        println("launchNotes - #${count.addAndGet(1)} isFrozen = ${actNotes.isFrozen}")
+                        println("launchNotes - #${count.value} onEach = $actNotes")
+                        if (actNotes.isNotEmpty()) {
+                            println("launchNotes - #${count.value} assertEquals")
+                            assertEquals(notes, actNotes)
+                        }
+                    }
+                }.catch { throwable ->
+                    withContext(this@runBlocking.coroutineContext) {
+                        throw Throwable("catch flow error: ${throwable.message}", throwable)
+                    }
+                }.launchIn(this@withContext)
+            }.join()
+        }
     }
 
     @Test
