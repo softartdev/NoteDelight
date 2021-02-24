9585f0b97b5d0b3bacbb796ec80900eefb24e8ec
artur <artik222012@gmail.com>
2021-02-24 09:28:56 +0500

    temp fix no act for exp

--- shared/src/commonTest/kotlin/com/softartdev/notedelight/shared/BaseTest.kt
+++ shared/src/commonTest/kotlin/com/softartdev/notedelight/shared/BaseTest.kt
@@ -3,6 +3,7 @@ package com.softartdev.notedelight.shared
 import com.softartdev.notedelight.shared.database.DatabaseRepo
 import kotlinx.coroutines.CoroutineScope
 
+@Suppress("NO_ACTUAL_FOR_EXPECT")//TODO remove when fixed
 expect abstract class BaseTest() {
 
     val dbRepo: DatabaseRepo
