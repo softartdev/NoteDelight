ade1a14d125e2baed7358758e53fa1e60c5b1860
artur <artik222012@gmail.com>
2021-04-01 15:36:25 +0500

    ✅ cipher from ktn-lib instead cocoapods 🎉

--- cipher-delight/build.gradle.kts
+++ cipher-delight/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     kotlin("multiplatform")
-    kotlin("native.cocoapods")
 }
 group = "com.softartdev"
 version = "0.1"
@@ -21,12 +20,10 @@ kotlin {
                 implementation(kotlin("test-annotations-common"))
             }
         }
-    }
-    cocoapods {
-        frameworkName = "CipherDelight"
-        summary = "Cipher library for the apps with SQLDelight"
-        homepage = "https://github.com/softartdev/NoteDelight"
-        ios.deploymentTarget = "13.5"
-        pod("SQLCipher", "~> 4.0")
+        val iosMain by getting {
+            dependencies {
+                api("io.github.softartdev:sqlcipher-ktn-pod:0.1")
+            }
+        }
     }
 }
