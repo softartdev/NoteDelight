c3b73f3481756b5b92685280889a9e10f0a6b15e
artur <artik222012@gmail.com>
2021-04-11 05:25:48 +0500

    prepare for merge to dev

--- cipher-delight/build.gradle.kts
+++ cipher-delight/build.gradle.kts
@@ -1,16 +1,17 @@
 plugins {
     kotlin("multiplatform")
-    kotlin("native.cocoapods")
 }
 group = "com.softartdev"
 version = "0.1"
 
 kotlin {
+    jvm()
     ios()
     sourceSets {
         val commonMain by getting {
             dependencies {
                 implementation(kotlin("stdlib-common"))
+                api("com.squareup.okio:okio-multiplatform:2.9.0")
             }
         }
         val commonTest by getting {
@@ -19,13 +20,10 @@ kotlin {
                 implementation(kotlin("test-annotations-common"))
             }
         }
-    }
-    cocoapods {
-        frameworkName = "CipherDelight"
-        summary = "Cipher library for the apps with SQLDelight"
-        homepage = "https://github.com/softartdev/NoteDelight"
-        ios.deploymentTarget = "13.5"
-        pod("SQLCipher", "~> 4.4.2")
-        useLibraries()
+        val iosMain by getting {
+            dependencies {
+                api("io.github.softartdev:sqlcipher-ktn-pod:1.0")
+            }
+        }
     }
 }
