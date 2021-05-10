30431f4cfa0759cbc0df9472f2bdac792bef36b9
artur <artik222012@gmail.com>
2021-05-10 08:48:52 +0500

    use cinterop from library

--- cipher-delight/build.gradle.kts
+++ cipher-delight/build.gradle.kts
@@ -4,23 +4,9 @@ plugins {
 group = "com.softartdev"
 version = "0.1"
 
-fun configInterop(target: org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget) {
-    val main by target.compilations.getting
-    val sqlite3 by main.cinterops.creating {
-        includeDirs("$projectDir/src/include")
-    }
-}
-
 kotlin {
     jvm()
-    val knTarget = iosX64("ios")
-
-    configInterop(knTarget)
-
-    knTarget.let { target ->
-        val test by target.compilations.getting
-        test.kotlinOptions.freeCompilerArgs += listOf("-linker-options", "-lsqlite3")
-    }
+    iosX64("ios")
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -36,7 +22,7 @@ kotlin {
         }
         val iosMain by getting {
             dependencies {
-//                api("io.github.softartdev:sqlcipher-ktn-pod:1.0")
+                api("io.github.softartdev:sqlcipher-ktn-pod:1.1")
 //                implementation("co.touchlab:sqliter:1.0.0")
             }
         }
