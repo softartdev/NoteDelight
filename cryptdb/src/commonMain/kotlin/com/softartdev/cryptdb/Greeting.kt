e0487d17e3d91a40fffe34929d8c84f15d316905
artur <artik222012@gmail.com>
2021-05-22 04:23:16 +0500

    remove new cryptdb module

--- cryptdb/src/commonMain/kotlin/com/softartdev/cryptdb/Greeting.kt
+++
@@ -1,7 +0,0 @@
-package com.softartdev.cryptdb
-
-class Greeting {
-    fun greeting(): String {
-        return "Hello, ${Platform().platform}!"
-    }
-}
--- cryptdb/src/commonMain/kotlin/com/softartdev/cryptdb/Platform.kt
+++
@@ -1,5 +0,0 @@
-package com.softartdev.cryptdb
-
-expect class Platform() {
-    val platform: String
-}
