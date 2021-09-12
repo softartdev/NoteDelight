0edd6eae87a2322ce72b54f0440004ea256bcd14
artur <artik222012@gmail.com>
2021-09-12 12:54:27 +0500

    fix jdbc holder for unit tests

--- shared/src/jvmMain/kotlin/com/softartdev/notedelight/shared/database/JdbcDatabaseHolder.kt
+++ shared/src/jvmMain/kotlin/com/softartdev/notedelight/shared/database/JdbcDatabaseHolder.kt
@@ -1,17 +1,32 @@
 package com.softartdev.notedelight.shared.database
 
 import com.softartdev.notedelight.shared.db.NoteDb
+import com.squareup.sqldelight.db.SqlCursor
+import com.squareup.sqldelight.db.use
 import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
 
-class JdbcDatabaseHolder: DatabaseHolder() {
+class JdbcDatabaseHolder : DatabaseHolder() {
     override val driver = JdbcSqliteDriver(
-        url = "jdbc:sqlite:${DatabaseRepo.DB_NAME}"
+        url = JdbcSqliteDriver.IN_MEMORY + DatabaseRepo.DB_NAME // jdbc:sqlite:notes.db
     )
     override val noteDb: NoteDb = createQueryWrapper(driver)
     override val noteQueries = noteDb.noteQueries
 
+    private var currentVersion: Int
+        get() {
+            val sqlCursor: SqlCursor = driver.executeQuery(null, "PRAGMA user_version;", 0, null)
+            val ver: Long? = sqlCursor.use { it.getLong(0) }
+            return ver!!.toInt()
+        }
+        set(value) = driver.execute(null, "PRAGMA user_version = $value;", 0, null)
+
     init {
-        if (NoteDb.Schema.version == 0) NoteDb.Schema.create(driver)
+        if (currentVersion == 0) {
+            NoteDb.Schema.create(driver)
+            currentVersion = 1
+        } else if (NoteDb.Schema.version > currentVersion) {
+            NoteDb.Schema.migrate(driver, currentVersion, NoteDb.Schema.version)
+        }
     }
 
     override fun close() = driver.close()
