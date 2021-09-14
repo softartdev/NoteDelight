2750538a287c08c863f5164651e2598179d9ae12
artur <artik222012@gmail.com>
2021-09-14 19:22:50 +0500

    update jb compose version

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
     kotlin("multiplatform")
-    id("org.jetbrains.compose") version "1.0.0-alpha4-build344"
+    id("org.jetbrains.compose") version "1.0.0-alpha4-build348"
 }
 
 group = "com.softartdev"
