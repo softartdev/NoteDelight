4db1575e6c0f6a2206ec82f0e01cd5a75b01bcaa
artur <artik222012@gmail.com>
2021-10-31 04:03:35 +0500

    fix koin update

--- androidApp/src/main/java/com/softartdev/notedelight/ui/base/BaseDialogFragment.kt
+++ androidApp/src/main/java/com/softartdev/notedelight/ui/base/BaseDialogFragment.kt
@@ -18,7 +18,6 @@ import org.koin.android.scope.AndroidScopeComponent
 import org.koin.androidx.scope.fragmentScope
 import org.koin.androidx.viewmodel.ViewModelOwner
 import org.koin.androidx.viewmodel.ViewModelOwnerDefinition
-import org.koin.androidx.viewmodel.scope.BundleDefinition
 import org.koin.androidx.viewmodel.scope.getViewModel
 import org.koin.core.parameter.ParametersDefinition
 import org.koin.core.qualifier.Qualifier
@@ -89,11 +88,10 @@ abstract class BaseDialogFragment(
 
     inline fun <reified T : ViewModel> viewModel(
         qualifier: Qualifier? = null,
-        noinline state: BundleDefinition? = null,
         noinline owner: ViewModelOwnerDefinition = { ViewModelOwner.from(this, this) },
         noinline parameters: ParametersDefinition? = null
     ): Lazy<T> = lazy(LazyThreadSafetyMode.NONE) {
-        scope.getViewModel(qualifier, state, owner, T::class, parameters)
+        scope.getViewModel(qualifier, owner, T::class, parameters)
     }
 
     companion object {
--- androidApp/src/main/java/com/softartdev/notedelight/ui/base/BasePrefFragment.kt
+++ androidApp/src/main/java/com/softartdev/notedelight/ui/base/BasePrefFragment.kt
@@ -8,7 +8,6 @@ import org.koin.android.scope.AndroidScopeComponent
 import org.koin.androidx.scope.fragmentScope
 import org.koin.androidx.viewmodel.ViewModelOwner
 import org.koin.androidx.viewmodel.ViewModelOwnerDefinition
-import org.koin.androidx.viewmodel.scope.BundleDefinition
 import org.koin.androidx.viewmodel.scope.getViewModel
 import org.koin.core.parameter.ParametersDefinition
 import org.koin.core.qualifier.Qualifier
@@ -25,10 +24,9 @@ abstract class BasePrefFragment : PreferenceFragmentCompat(), AndroidScopeCompon
 
     inline fun <reified T : ViewModel> viewModel(
         qualifier: Qualifier? = null,
-        noinline state: BundleDefinition? = null,
         noinline owner: ViewModelOwnerDefinition = { ViewModelOwner.from(this, this) },
         noinline parameters: ParametersDefinition? = null
     ): Lazy<T> = lazy(LazyThreadSafetyMode.NONE) {
-        scope.getViewModel(qualifier, state, owner, T::class, parameters)
+        scope.getViewModel(qualifier, owner, T::class, parameters)
     }
 }
