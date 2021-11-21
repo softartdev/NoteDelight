8dd92763febab84f7c7dd18ac51e9b3a43f4d3bf
artur <artik222012@gmail.com>
2021-11-21 06:13:34 +0500

    propagate jb-compose version by classpath dependency of buildscript

--- shared-compose-ui/build.gradle.kts
+++ shared-compose-ui/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.compose.compose
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
-    id("org.jetbrains.compose") version "1.0.0-beta5"
+    id("org.jetbrains.compose")
 }
 kotlin {
     jvm()
@@ -16,6 +16,7 @@ kotlin {
                 implementation(compose.runtime)
                 implementation(compose.foundation)
                 implementation(compose.material)
+                implementation(compose.desktop.common)
                 implementation("org.jetbrains.compose.material:material:${org.jetbrains.compose.ComposeBuildConfig.composeVersion}")
                 implementation("org.jetbrains.compose.material:material-icons-extended:${org.jetbrains.compose.ComposeBuildConfig.composeVersion}")
                 val decomposeVersion = "0.3.1"
