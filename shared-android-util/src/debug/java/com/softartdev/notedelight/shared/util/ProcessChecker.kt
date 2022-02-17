6fcf95cb7738f3d539cafef1cccb816c63058c6a
Artur Babichev <artik222012@gmail.com>
2022-02-17 05:40:07 +0500

    leak canary: obtain by reflection, keep standard dependency for work-manager

--- shared-android-util/src/debug/java/com/softartdev/notedelight/shared/util/ProcessChecker.kt
+++
@@ -1,8 +0,0 @@
-package com.softartdev.notedelight.shared.util
-
-import android.content.Context
-import leakcanary.LeakCanaryProcess
-
-
-val Context.isInLeakCanaryAnalyzerProcess: Boolean
-    get() = LeakCanaryProcess.isInAnalyzerProcess(this)
--- shared-android-util/src/release/java/com/softartdev/notedelight/shared/util/ProcessChecker.kt
+++
@@ -1,7 +0,0 @@
-package com.softartdev.notedelight.shared.util
-
-import android.content.Context
-
-
-val Context.isInLeakCanaryAnalyzerProcess: Boolean
-    get() = false
