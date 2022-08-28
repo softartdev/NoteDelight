9ac87bc52c29395101e480a99b7e7ebfe1d361c5
Artur Babichev <artik222012@gmail.com>
2022-08-28 21:30:06 +0400

    add remaining ios targets

--- ios-compose-app/build.gradle.kts
+++ ios-compose-app/build.gradle.kts
@@ -1,23 +1,29 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
 plugins {
     kotlin("multiplatform")
     id("org.jetbrains.compose")
     id("dev.icerock.mobile.multiplatform-resources")
 }
 
-kotlin {
-    iosX64("uikitX64") {
-        binaries {
-            executable {
-                entryPoint = "com.softartdev.notedelight.main"
-                freeCompilerArgs += listOf(
-                    "-linker-option", "-framework", "-linker-option", "Metal",
-                    "-linker-option", "-framework", "-linker-option", "CoreText",
-                    "-linker-option", "-framework", "-linker-option", "CoreGraphics"
-                )
-                linkerOpts.add("-lsqlite3")
-            }
+val binConfig: KotlinNativeTarget.() -> Unit = {
+    binaries {
+        executable {
+            entryPoint = "com.softartdev.notedelight.main"
+            freeCompilerArgs += listOf(
+                "-linker-option", "-framework", "-linker-option", "Metal",
+                "-linker-option", "-framework", "-linker-option", "CoreText",
+                "-linker-option", "-framework", "-linker-option", "CoreGraphics"
+            )
+            linkerOpts.add("-lsqlite3")
         }
     }
+}
+
+kotlin {
+    iosX64("uikitX64", binConfig)
+    iosArm64("uikitArm64", binConfig)
+//    iosSimulatorArm64("uikitSimulatorArm64", binConfig)
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -31,10 +37,21 @@ kotlin {
                 implementation(libs.koin.core)
             }
         }
+        val uikitX64Main by getting
+        val uikitArm64Main by getting
+//        val uikitSimulatorArm64Main by getting
+        val uikitMain by creating {
+            dependsOn(commonMain)
+            uikitX64Main.dependsOn(this)
+            uikitArm64Main.dependsOn(this)
+//            uikitSimulatorArm64Main.dependsOn(this)
+            dependencies {
+            }
+        }
     }
 }
 
-kotlin.targets.withType<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget> {
+kotlin.targets.withType<KotlinNativeTarget> {
     binaries.all {
         binaryOptions["memoryModel"] = "experimental"
         binaryOptions["freezing"] = "disabled"
@@ -42,7 +59,7 @@ kotlin.targets.withType<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarge
 }
 
 kotlin {
-    targets.withType<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget> {
+    targets.withType<KotlinNativeTarget> {
         binaries.all {
             // TODO: the current compose binary surprises LLVM, so disable checks for now.
             freeCompilerArgs += "-Xdisable-phases=VerifyBitcode"
--- shared-compose-ui/build.gradle.kts
+++ shared-compose-ui/build.gradle.kts
@@ -17,7 +17,9 @@ kotlin {
         }
     }
     android()
-    iosX64("ios")
+    iosX64()
+    iosArm64()
+//    iosSimulatorArm64()
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -53,8 +55,17 @@ kotlin {
                 implementation(compose.uiTestJUnit4)
             }
         }
-        val iosMain by getting
-        val iosTest by getting
+        val iosX64Main by getting
+        val iosArm64Main by getting
+//        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+//            iosSimulatorArm64Main.dependsOn(this)
+            dependencies {
+            }
+        }
     }
 }
 
