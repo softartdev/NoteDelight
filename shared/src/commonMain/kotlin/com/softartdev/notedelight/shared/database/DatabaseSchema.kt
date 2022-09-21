0a1bf9ce7dee9b50ed01d8614ed36392118247ef
Artur Babichev <artik222012@gmail.com>
2022-09-22 02:27:08 +0400

    fix test data

--- shared/src/commonMain/kotlin/com/softartdev/notedelight/shared/database/DatabaseSchema.kt
+++ shared/src/commonMain/kotlin/com/softartdev/notedelight/shared/database/DatabaseSchema.kt
@@ -1,36 +1,42 @@
 package com.softartdev.notedelight.shared.database
 
 import com.softartdev.notedelight.shared.date.DateAdapter
-import com.softartdev.notedelight.shared.date.getSystemTimeInMillis
 import com.softartdev.notedelight.shared.db.Note
 import com.softartdev.notedelight.shared.db.NoteDb
 import com.softartdev.notedelight.shared.db.NoteQueries
 import com.squareup.sqldelight.db.SqlDriver
-import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDateTime
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toLocalDateTime
 
 fun createQueryWrapper(sqlDriver: SqlDriver): NoteDb {
     val dateColumnAdapter = DateAdapter()
     val noteColumnAdapter = Note.Adapter(dateColumnAdapter, dateColumnAdapter)
-    return NoteDb(
-        driver = sqlDriver,
-        noteAdapter = noteColumnAdapter
-    )
+    return NoteDb(driver = sqlDriver, noteAdapter = noteColumnAdapter)
 }
 
 object TestSchema {
-    private val ldt: LocalDateTime//FIXME cannot use createLocalDateTime() because nanoseconds don't save to db therefore tests fail
-        get() = Instant.fromEpochMilliseconds(
-            epochMilliseconds = getSystemTimeInMillis()
-        ).toLocalDateTime(TimeZone.currentSystemDefault())
 
-    val firstNote = Note(1, "first title from test schema", "first text", ldt, ldt)
-    val secondNote = Note(2, "second title", "second text", ldt, ldt)
-    val thirdNote = Note(3, "third title", "third text", ldt, ldt)
+    val firstNote = Note(
+        id = 1,
+        title = "first title from test schema",
+        text = "first text",
+        dateCreated = LocalDateTime(2018, 1, 2, 3, 4),
+        dateModified = LocalDateTime(2018, 2, 3, 4, 5),
+    )
+    val secondNote = Note(
+        id = 2,
+        title = "second title",
+        text = "second text",
+        dateCreated = LocalDateTime(2017, 1, 2, 3, 4),
+        dateModified = LocalDateTime(2017, 2, 3, 4, 5),
+    )
+    val thirdNote = Note(
+        id = 3,
+        title = "third title",
+        text = "third text",
+        dateCreated = LocalDateTime(2016, 1, 2, 3, 4),
+        dateModified = LocalDateTime(2016, 2, 3, 4, 5),
+    )
 
     fun insertTestNotes(noteQueries: NoteQueries) =
-        sequenceOf(firstNote, secondNote, thirdNote)
-            .forEach(noteQueries::insert)
+        sequenceOf(firstNote, secondNote, thirdNote).forEach(noteQueries::insert)
 }
