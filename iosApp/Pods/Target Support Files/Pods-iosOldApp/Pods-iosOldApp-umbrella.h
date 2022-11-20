f94a8ca61682a2e34a9e2de905e548e3e43eb683
Artur Babichev <artik222012@gmail.com>
2022-11-21 02:57:32 +0400

    run ios-compose-app on ipad

--- iosApp/Pods/Target Support Files/Pods-iosOldApp/Pods-iosOldApp-umbrella.h
+++ iosApp/Pods/Target Support Files/Pods-iosOldApp/Pods-iosOldApp-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_iosAppVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_iosAppVersionString[];
+FOUNDATION_EXPORT double Pods_iosOldAppVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_iosOldAppVersionString[];
 
