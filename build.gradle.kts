1ee2fc9d14f854578a771d8bafb3e7c3fb49bf02
Artur Babichev <artik222012@gmail.com>
2022-12-15 11:10:21 +0400

    update libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,10 @@ buildscript {
         classpath("com.android.tools.build:gradle:7.3.1")
         classpath("com.google.gms:google-services:4.3.14")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
-        classpath("com.google.android.gms:oss-licenses-plugin:0.10.5")
+        classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
         classpath("com.slack.keeper:keeper:0.12.0")
         classpath(libs.mokoResources.generator)
-        classpath("org.jetbrains.compose:compose-gradle-plugin:1.2.1")
+        classpath("org.jetbrains.compose:compose-gradle-plugin:1.2.2")
     }
 }
 
@@ -23,6 +23,7 @@ allprojects {
         mavenCentral()
         maven(url = "https://jitpack.io")
         maven(url = "https://s3.amazonaws.com/repo.commonsware.com")
+        maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
         google()
+        maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 rootProject.name = "NoteDelight"
