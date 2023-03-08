97b9a7f939c6e0d2b928981e7e731899f0913ce8
Artur Babichev <artik222012@gmail.com>
2023-03-08 18:57:03 +0400

    update libs

--- ios-compose-kit/src/commonMain/kotlin/com/softartdev/notedelight/SkikoHelper.kt
+++ ios-compose-kit/src/commonMain/kotlin/com/softartdev/notedelight/SkikoHelper.kt
@@ -1,6 +1,6 @@
 package com.softartdev.notedelight
 
-import androidx.compose.ui.window.Application
+import androidx.compose.ui.window.ComposeUIViewController
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import com.arkivanov.essenty.lifecycle.destroy
@@ -8,7 +8,6 @@ import com.arkivanov.essenty.lifecycle.resume
 import com.arkivanov.essenty.lifecycle.stop
 import com.softartdev.notedelight.shared.di.allModules
 import com.softartdev.notedelight.ui.MainRootUI
-import dev.icerock.moko.resources.desc.desc
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
 import org.koin.core.context.startKoin
@@ -18,10 +17,7 @@ import platform.UIKit.UIViewController
 class SkikoHelper {
     val lifecycle = LifecycleRegistry()
     val root = RootComponent(DefaultComponentContext(lifecycle))
-    val applicationUIViewController: UIViewController = Application(
-        title = MR.strings.app_name.desc().localized(),
-        content = { MainRootUI(root) }
-    )
+    val applicationUIViewController: UIViewController = ComposeUIViewController { MainRootUI(root) }
 
     fun appInit() {
         Napier.base(antilog = DebugAntilog())
