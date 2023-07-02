b78b24d517449e97edd5ed95d84d6d9d29e5d9ac
Artur Babichev <artik222012@gmail.com>
2023-07-02 05:25:29 +0400

    update libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,12 +8,12 @@ buildscript {
     dependencies {
         classpath(kotlin("gradle-plugin", libs.versions.kotlin.get()))
         classpath(libs.sqlDelight.gradlePlugin)
-        classpath("com.android.tools.build:gradle:8.0.1")
+        classpath("com.android.tools.build:gradle:8.0.2")
         classpath("com.google.gms:google-services:4.3.15")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.5")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.6")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
         classpath(libs.mokoResources.generator)
-        classpath("org.jetbrains.compose:compose-gradle-plugin:1.4.0")
+        classpath("org.jetbrains.compose:compose-gradle-plugin:1.4.1")
     }
 }
 
