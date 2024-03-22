ab03797d3e965ca7506842169087cc4fe0aa16c4
babichev.a <babichev.a@promail366.com>
2024-03-22 18:51:58 +0400

    Testing Compose Multiplatform UI

--- shared-compose-ui/src/commonTest/kotlin/com/softartdev/netedelight/ExampleTest.kt
+++ shared-compose-ui/src/commonTest/kotlin/com/softartdev/netedelight/ExampleTest.kt
@@ -1,10 +1,18 @@
 package com.softartdev.netedelight
 
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.test.*
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.runComposeUiTest
 import kotlin.test.Test
 
 class ExampleTest {
