3c9bafaba00b800f1c7a6b447e4150aebdd160de
Artur Babichev <artik222012@gmail.com>
2024-09-04 22:27:02 +0400

    use vector icon on splash screen

--- desktop-compose-app/src/jvmMain/kotlin/com/softartdev/notedelight/Main.kt
+++ desktop-compose-app/src/jvmMain/kotlin/com/softartdev/notedelight/Main.kt
@@ -1,5 +1,7 @@
 package com.softartdev.notedelight
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
@@ -8,12 +10,11 @@ import com.softartdev.notedelight.di.navigationModule
 import com.softartdev.notedelight.shared.di.sharedModules
 import com.softartdev.notedelight.shared.navigation.Router
 import com.softartdev.notedelight.shared.util.NapierKoinLogger
+import com.softartdev.notedelight.ui.icon.FileLock
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
 import notedelight.shared_compose_ui.generated.resources.Res
-import notedelight.shared_compose_ui.generated.resources.app_icon
 import notedelight.shared_compose_ui.generated.resources.app_name
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
@@ -32,7 +33,7 @@ fun main() {
             onCloseRequest = ::exitApplication,
             title = stringResource(Res.string.app_name),
             state = rememberWindowState(width = 320.dp, height = 480.dp),
-            icon = painterResource(Res.drawable.app_icon)
+            icon = rememberVectorPainter(image = Icons.Filled.FileLock),
         ) {
             CustomDesktopTheme {
                 App(router)
