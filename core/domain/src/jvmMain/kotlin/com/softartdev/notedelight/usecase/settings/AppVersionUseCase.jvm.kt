3e797060e160ebcff6d9cadc3967794b77081c4a
Artur Babichev <artik222012@gmail.com>
2026-03-09 06:52:48 +0400

    build: Update app version retrieval on JVM and update iOS state
    - Refactor `AppVersionUseCase.jvm.kt` to retrieve the implementation version using the system class loader for the specific package.
    - Add logging to `AppVersionUseCase` on JVM using Kermit.
    - Update iOS user interface state in `.xcuserstate`.

--- core/domain/src/jvmMain/kotlin/com/softartdev/notedelight/usecase/settings/AppVersionUseCase.jvm.kt
+++ core/domain/src/jvmMain/kotlin/com/softartdev/notedelight/usecase/settings/AppVersionUseCase.jvm.kt
@@ -1,8 +1,13 @@
 package com.softartdev.notedelight.usecase.settings
 
+import co.touchlab.kermit.Logger
+
 actual class AppVersionUseCase {
+    private val logger = Logger.withTag("AppVersionUseCase")
 
     actual operator fun invoke(): String? {
-        return javaClass.`package`?.implementationVersion?.takeIf(String::isNotBlank)
+        val pkg: Package? = ClassLoader.getSystemClassLoader().getDefinedPackage("com.softartdev.notedelight")
+        logger.i(message = pkg::toString)
+        return pkg?.implementationVersion?.takeIf(String::isNotBlank)
     }
 }
